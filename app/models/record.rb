a45fb82be33b70d4a48f256bcf9c524e7448993f
yasinabdmahmood <abd181103@gmail.com>
2022-12-26 19:30:38 +0300

    Add validation rules to Record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,5 +2,13 @@ class Record < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :catagory_records, dependent: :destroy
-  has_many :catagories, through: :catagory_records
+  has_many :catagories, through: :catagory_record
+
+   # validationd
+   validates :name, presence: true
+   validates :amount, presence: true
+   validates :amount, numericality: true
+   validates :amount, numericality:{ greater_than : 0 }
+   validates :name, length: { maximum: 100 }
+   validates_associated :catagory_records
 end
