a9ef2e8e3ed8409ef1884ef0e704d711dba205b6
yasinabdmahmood <abd181103@gmail.com>
2022-12-26 15:17:23 +0300

    Add association between records table and catagories tables

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,6 @@
 class Record < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+
+    has_many :catagory_records, dependent: :destroy
+    has_many :catagories, through: :catagory_records
 end
--- db/migrate/20221226115117_create_catagory_records.rb
+++ db/migrate/20221226115117_create_catagory_records.rb
@@ -1,6 +1,8 @@
 class CreateCatagoryRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :catagory_records do |t|
+      t.references :record, null: false, foreign_key: true
+      t.references :catagory, null: false, foreign_key: true
 
       t.timestamps
     end
