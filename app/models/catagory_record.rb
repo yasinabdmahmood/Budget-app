74592961af2bd6bacb1e94f2e30afa80c85f53ed
yasinabdmahmood <abd181103@gmail.com>
2022-12-26 19:07:48 +0300

    Fix linter errors

--- app/models/catagory_record.rb
+++ app/models/catagory_record.rb
@@ -1,4 +1,4 @@
 class CatagoryRecord < ApplicationRecord
-    belongs_to :record
-    belongs_to :catagory
+  belongs_to :record
+  belongs_to :catagory
 end
--- test/models/catagory_test.rb
+++ test/models/catagory_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class CatagoryTest < ActiveSupport::TestCase
   # test "the truth" do
