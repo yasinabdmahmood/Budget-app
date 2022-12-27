e16bff0879f1da9786c20ddffaddd7d098600fab
yasinabdmahmood <abd181103@gmail.com>
2022-12-27 15:41:29 +0300

    Add root path in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'greetings/hello'
+  root to: "greetings#hello"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 
