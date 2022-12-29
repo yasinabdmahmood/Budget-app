24a554c5f8d09a17881bcfa9c32c849c74b656ba
yasinabdmahmood <abd181103@gmail.com>
2022-12-29 14:08:08 +0300

    Fixed catagory choosing issue in transaction form

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,17 +1,18 @@
 class TransactionController < ApplicationController
   def index
     @records = current_user.records.select { |record| record.catagory_records[0].catagory.id == params[:id].to_i }
+    @catagory_id = params[:id]
   end
 
   def create
     @new_record = Record.new(name: transaction_params[:name], amount: transaction_params[:amount])
     @new_record.author = current_user
     @new_record.save
-    @selected_catagory = Catagory.find(transaction_params[:catagory])
+    @selected_catagory = Catagory.find(transaction_params[:catagory_id])
     @new_catagory_record = CatagoryRecord.new(record: @new_record, catagory: @selected_catagory)
     @new_catagory_record.save
     if @new_catagory_record.save && @new_record.save
-      redirect_to "/transaction/index/#{transaction_params[:catagory]}"
+      redirect_to "/transaction/index/#{transaction_params[:catagory_id]}"
     else
       render :new
     end
@@ -20,11 +21,12 @@ class TransactionController < ApplicationController
   def new
     @new_record = Record.new
     @catagories = current_user.catagories.map { |catagory| [catagory.name, catagory.id] }
+    @catagory_id = params[:id]
   end
 
   private
 
   def transaction_params
-    params.require(:new_record).permit(:name, :amount, :catagory)
+    params.require(:new_record).permit(:name, :amount, :catagory_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'transaction/index/:id', to: 'transaction#index', as: 'transaction_index_path'
   post 'transaction/create'
-  get 'transaction/new'
+  get 'transaction/new/:id', to: 'transaction#new', as: 'transaction_new_path'
   get 'catagory/index'
   get 'catagory/new'
   post 'catagory/create'
