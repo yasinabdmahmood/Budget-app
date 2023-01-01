9596ca1a48ff838387954ae9eaa8ecf63dffda18
yasinabdmahmood <abd181103@gmail.com>
2023-01-02 01:05:29 +0300

    Add test for catagory#new view

--- spec/views/catagory_new_spec.rb
+++ spec/views/catagory_new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Transaction new page', type: :feature do
+RSpec.describe 'catagory new page', type: :feature do
   before(:example) do
     @user = User.create(name: 'Yaseen', email: 'test@test.com', password: 'password', password_confirmation: 'password')
     @category1 = Catagory.create(name: 'category_1', icon: 'http:/xyz', author: @user)
@@ -9,12 +9,17 @@ RSpec.describe 'Transaction new page', type: :feature do
     @catagory_record1 = CatagoryRecord.create(record: @record1, catagory: @category1)
 
     sign_in @user
-    visit transaction_new_path_url(id: @category1.id)
+    visit catagory_new_path
   end
 
-  describe 'transaction new page' do
-    it ' -> I can see the phrase Create new transaction on the page' do
-      expect(page).to have_content('Create new transaction')
+  describe 'catagory new page' do
+    it ' -> I can see fields name and icon on the page' do
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Icon')
+    end
+
+    it ' -> I can see the Logout button ' do
+      expect(page).to have_content('Logout')
     end
   end
 end
