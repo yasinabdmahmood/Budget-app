74475d714456f17f8c5e918df4984ad524d5cb4c
yasinabdmahmood <abd181103@gmail.com>
2023-01-01 22:54:55 +0300

    Add test for Catagory controller

---
+++ spec/requests/catagory_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'Catagory', type: :request do
+  describe 'GET /new' do
+    before(:example) { get '/catagory/new' }
+    it 'redirects' do
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe 'GET /index' do
+    before(:example) { get '/catagory/index' }
+    it 'redirects' do
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe 'GET /create' do
+    before(:example) { post '/catagory/create' }
+    it 'redirects' do
+      expect(response).to have_http_status(:found)
+    end
+  end
+end
