f87dea4ed923d753766b5f3dff30f02cc65ca664
yasinabdmahmood <abd181103@gmail.com>
2023-01-01 22:59:54 +0300

    Add test for transaction_controller

--- spec/requests/transaction_controller_spec.rb
+++ spec/requests/transaction_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
-RSpec.describe 'Catagory', type: :request do
-  describe 'GET /new' do
-    before(:example) { get '/catagory/new' }
+RSpec.describe 'Transaction', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/transaction/index/:id' }
     it 'redirects' do
       expect(response).to have_http_status(:found)
     end
   end
 
-  describe 'GET /index' do
-    before(:example) { get '/catagory/index' }
+  describe 'GET /new' do
+    before(:example) { get '/transaction/new/:id' }
     it 'redirects' do
       expect(response).to have_http_status(:found)
     end
   end
 
   describe 'POST /create' do
-    before(:example) { post '/catagory/create' }
+    before(:example) { post '/transaction/create' }
     it 'redirects' do
       expect(response).to have_http_status(:found)
     end
