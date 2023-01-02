ab00cb4edb2a9e920ce21f00348446981fd5a164
yasinabdmahmood <abd181103@gmail.com>
2023-01-02 18:47:09 +0300

    Delete render.yaml file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
-#!usr/bin/env bash 
-# exit on error 
-set -o errexit 
-bundle install 
-bundle exec rails assets:precompile 
-bundle exec rails assets:clean 
-bundle exec rails db:migrate 
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:create
+bundle exec rake db:migrate
