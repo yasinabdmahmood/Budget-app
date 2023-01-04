be38d2163d954ec12628b66649854804bb33ab95
yasinabdmahmood <abd181103@gmail.com>
2023-01-04 11:42:47 +0300

    Add a message to by displayed on transactions page for empty transaction list

--- config/puma.rb
+++ config/puma.rb
@@ -31,7 +31,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 
-#workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 
 # Use the `preload_app!` method when specifying a `workers` number.
